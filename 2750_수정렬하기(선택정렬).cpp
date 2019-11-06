463300856e61df0e04a29f619e776ee01c615f42
KiHyeonYun <ykh094@naver.com>
2019-11-06 22:44:11 +0900

    2019-11-06

---
+++ 2750_수정렬하기(선택정렬).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int* selection_sort(int *arr, int length);
+
+int main(){
+
+	int* arr;
+	int n;
+
+	cin>>n;
+	arr = new int [n];
+
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	arr = selection_sort(arr, n);
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<'\n';
+	}
+
+	return 0;
+}
+int* selection_sort(int *arr, int length){
+	int least,temp;
+
+	for(int i=0; i<length-1; i++){
+			least = i;
+		for(int j=i+1; j<length; j++){
+			if(arr[least] > arr[j])
+				least = j;
+		} 
+		//swap
+		temp = arr[i];
+		arr[i] = arr[least];
+		arr[least] = temp;
+	}
+	return arr;
+}
